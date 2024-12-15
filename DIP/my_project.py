99edcfcea0a9361154efb380d17b082d50153b32
KYashin <128860411+KYashin@users.noreply.github.com>
2024-12-15 21:26:21 +0300

    Разработка my_program.py.

--- DIP/my_project.py
+++ DIP/my_project.py
@@ -1859,7 +1859,8 @@ class MainWindow(QMainWindow):
             while True:
                     success, frame = cap.read()
                     frame_1 = frame.astype(np.int32)
-                    frame_1 = 255 - frame                    frame_1 = frame_1.astype(np.uint8)
+                    frame_1 = 255 - frame
+                    frame_1 = frame_1.astype(np.uint8)
 
                     cv2.imshow("Frame_1", frame_1)
                     cv2.imshow("Frame", frame)
